00ed7597c3236c91c785e39708be56589dcaa261
OloriAsabi <developerqueenlatifa@gmail.com>
2022-06-09 11:31:31 +0100

    commit on "main"

--- components/home/SearchBar.js
+++ components/home/SearchBar.js
@@ -8,7 +8,7 @@ export default function SearchBar({ cityHandler }) {
   return (
     <View style={{ marginTop: 15, flexDirection: "row" }}>
       <GooglePlacesAutocomplete
-        query={{ key: "AIzaSyD49RaCInIeqMhzJSceTBzHOYYYlXN4u3U" }}
+        query={{ key: process.env.GOOGLE_KEY }}
         onPress={(data, details = null) => {
           console.log(data.description);
           const city = data.description.split(",")[0];
